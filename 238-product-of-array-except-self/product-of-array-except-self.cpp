48707603e72352be45fb0eff6eb0c8404739be45
malikahere <93263000+malikahere@users.noreply.github.com>
2023-12-02 21:12:28 +0530

    Time: 17 ms (67.93%) | Memory: 24.3 MB (93.02%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -4,24 +4,17 @@ public:
            int n = nums.size();
     vector<int> result(n, 1);
 
-    
-    unordered_map<int, int> leftProduct, rightProduct;
-
     int ptol = 1;
     for (int i = 0; i < n; ++i) {
-        leftProduct[i] = ptol;
+        result[i] *= ptol;
         ptol *= nums[i];
     }
 
     
     int ptor = 1;
     for (int i = n - 1; i >= 0; --i) {
-        rightProduct[i] = ptor;
+        result[i]  *= ptor;
         ptor *= nums[i];
-    }
-
-    for (int i = 0; i < n; ++i) {
-        result[i] = leftProduct[i] * rightProduct[i];
     }
 
     return result;
