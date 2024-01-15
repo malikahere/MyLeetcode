af7ffcbf5c78a5edd74adb7c0fa787e1e0559ad9
malikahere <93263000+malikahere@users.noreply.github.com>
2024-01-15 23:41:15 +0530

    Time: 410 ms (93.62%) | Memory: 164.7 MB (74.48%) - LeetSync

--- 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.cpp
+++ 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    vector<vector<int>> findWinners(vector<vector<int>>& m) {
-      unordered_map<int, int> umap;
+    vector<vector<int>> findWinners(vector<vector<int>>& matches) {
+      unordered_map<int, int> mp;
         vector<int> v1, v2;
-        int n = m.size();
+        int n = matches.size();
         for(int i=0;i<n;i++) {
-            umap[m[i][0]];
-            umap[m[i][1]]++;
+            mp[matches[i][0]];
+            mp[matches[i][1]]++;
         }
-        for(auto x: umap){
+        for(auto x: mp){
             if(x.second==0) v1.push_back(x.first);
             else if(x.second==1) v2.push_back(x.first);
         }
