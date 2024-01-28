96ad8d4a7369f45dfa06092bf2e5250ab127e557
malikahere <93263000+malikahere@users.noreply.github.com>
2024-01-28 21:00:10 +0530

    Time: 429 ms (90.42%) | Memory: 164.6 MB (43.00%) - LeetSync

--- 1145-number-of-submatrices-that-sum-to-target/number-of-submatrices-that-sum-to-target.cpp
+++ 1145-number-of-submatrices-that-sum-to-target/number-of-submatrices-that-sum-to-target.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
     int numSubmatrixSumTarget(vector<vector<int>>& matrix, int target) {
-        int m = matrix.size();
+            int m = matrix.size();
     int n = matrix[0].size();
     
-    // Calculate prefix sums
-    vector<vector<int>> prefix_sums(m + 1, vector<int>(n + 1, 0));
-    for (int i = 1; i <= m; ++i) {
-        for (int j = 1; j <= n; ++j) {
-            prefix_sums[i][j] = matrix[i - 1][j - 1] + prefix_sums[i - 1][j] + prefix_sums[i][j - 1] - prefix_sums[i - 1][j - 1];
-        }
-    }
-    
     int count = 0;
-    for (int top_row = 1; top_row <= m; ++top_row) {
-        for (int bottom_row = top_row; bottom_row <= m; ++bottom_row) {
-            unordered_map<int, int> seen;
-            for (int col = 1; col <= n; ++col) {
-                int current_sum = prefix_sums[bottom_row][col] - prefix_sums[top_row - 1][col];
+    for (int top = 0; top < m; ++top) {
+        vector<int> row_sum(n, 0);
+        for (int bottom = top; bottom < m; ++bottom) {
+            unordered_map<int, int> prefix_sum_count;
+            int current_sum = 0;
+            for (int col = 0; col < n; ++col) {
+                row_sum[col] += matrix[bottom][col];
+                current_sum += row_sum[col];
                 if (current_sum == target)
                     count++;
-                if (seen.find(current_sum - target) != seen.end())
-                    count += seen[current_sum - target];
-                seen[current_sum]++;
+                if (prefix_sum_count.find(current_sum - target) != prefix_sum_count.end())
+                    count += prefix_sum_count[current_sum - target];
+                prefix_sum_count[current_sum]++;
             }
         }
     }
