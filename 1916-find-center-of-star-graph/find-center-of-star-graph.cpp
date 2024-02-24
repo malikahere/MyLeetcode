bad0b184ec82e25b037ce25a4188f0e5530183af
malikahere <93263000+malikahere@users.noreply.github.com>
2024-02-24 14:44:45 +0530

    Time: 147 ms (67.56%) | Memory: 80.6 MB (40.42%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     int findCenter(vector<vector<int>>& edges) {
         int n = edges.size();
-        vector<int> star (n+2,0);
-        for(auto x : edges){
-            int a = x[0];
-            int b = x[1];
-            star[a]++;
-            star[b]++;
+        vector<int> star(n + 2, 0);
+        for (auto x : edges) {
+            star[x[0]]++;
+            star[x[1]]++;
         }
-        for(int i =1 ; i<=n+1 ;i++ ){
-            if (star[i]==n) 
+        for (int i = 1; i <= n + 1; i++) {
+            if (star[i] == n)
 
-           { return i;}
+            {
+                return i;
+            }
         }
         return -1;
     }
