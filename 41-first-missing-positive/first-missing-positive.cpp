9e6fbdc84b0b823128cad15df84ca6b6221bf045
malikahere <93263000+malikahere@users.noreply.github.com>
2024-03-26 20:58:33 +0530

    Time: 131 ms (5.12%) | Memory: 60.7 MB (6.19%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -1,33 +1,17 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-          int n=nums.size();
-        for(int i=0; i<n; i++){
-            int element = nums[i];
-           
-            if(element >=1 && element <= n)
-            {
-            int chair = element-1;
-            if(nums[chair]!= element)
-            {
-                swap( nums[chair],nums[i]);
-                i--;
-               }
-            }
-        }
-      int i=0;
-      while(i<n)
-      { if(nums[i]==INT_MIN ) return i+1;
-        if( nums[i]>INT_MIN && nums[i]-1!=i)
+         int n=nums.size();
+        set<int>s;
+        for(int i=0;i<n;i++)
         {
-            return i+1;
+            s.insert(nums[i]);
         }
-        i++;
-      }
-      if(i>=n)
-      {
-        return i+1;
-      }
-     return 0;
+        for(int j=1;j<=n+1;j++)
+        {
+            if(s.find(j)==s.end())
+            return j;
+        }
+        return 0;
     }
 };
